271f1f854d67d10804d6e4ec26f2b3a53e9e8160
Malik Heron <malik.heron2001@gmail.com>
2025-10-18 15:52:09 -0500

    feat: update landing page layout and add performance metrics, create Docker configuration for NGINX with routing for backend and frontend services

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -14,7 +14,7 @@ export default function Landing() {
       <div className='flex grow p-6'>
         <main className="flex flex-row justify-center mx-auto w-full max-w-7xl gap-12">
           <div className="flex flex-col md:flex-row w-full gap-6">
-            <div className="flex flex-col justify-start max-w-md gap-4">
+            <div className="flex flex-col max-w-md gap-4">
               <TextAnimate animation="blurInUp" by="character" once className="text-3xl font-bold tracking-tight">
                 Master Every Rep with AI Precision.
               </TextAnimate>
@@ -28,11 +28,25 @@ export default function Landing() {
                 </Button>
                 <Button variant="secondary" size="lg">How it works</Button>
               </div>
-              <div className="bg-card flex flex-col p-4">
+              <div data-aos='flip-up' data-aos-delay={200} className="grid grid-cols-3 gap-4 mt-4">
+                <div className="bg-card flex flex-col p-4 rounded-sm">
+                  <p className="text-sm text-muted-foreground">Form Accuracy</p>
+                  <p className="text-xl font-bold">92%</p>
+                </div>
+                <div className="bg-card flex flex-col p-4 rounded-sm">
+                  <p className="text-sm text-muted-foreground">Avg. Rep Speed</p>
+                  <p className="text-xl font-bold">1.2s</p>
+                </div>
+                <div className="bg-card flex flex-col p-4 rounded-sm">
+                  <p className="text-sm text-muted-foreground">Tempo</p>
+                  <p className="text-xl font-bold text-green-300">Good</p>
+                </div>
               </div>
             </div>
             <img src="/pose_tracking.svg" alt="Pose Tracking Illustration" className="md:mx-auto md:max-w-lg" />
           </div>
+
+
         </main>
       </div>
     </>
